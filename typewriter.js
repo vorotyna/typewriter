675287dceca8460bba51352179c193f9861dc52f
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-25 21:58:37 +0000

    Using setTimeout and process.stdout.write

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs \n";
+let delay = 0;
+
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 50);
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
+
+
+
+
+
